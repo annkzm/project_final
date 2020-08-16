2cc97813673c3610266455953ef53f495f93b26f
annkzm <kozminykhann@gmail.com>
2020-08-16 10:32:46 +0300

    frst commit

---
+++ project_final_01/Clustering.c
@@ -0,0 +1,42 @@
+/*
+ ============================================================================
+ Name        : Clustering.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "SparseMatrix.h"
+#include "Common.h"
+#include "Utility.h"
+
+int main(int argc, char *argv[]) {
+	Status status = INVALID_STATUS_CODE;
+	double M, *k;
+	int n;
+	spmat *A ;
+	status = extract_matrix_size(argc, argv, &n);
+	A =spmat_allocate_list(n); /*allocation check within the function*/
+	if(NULL == A){
+		status = MALLOC_FAILED_CODE;
+		get_error_message(status);
+		goto l_cleanup;
+	}
+	k = (double*)malloc(n * sizeof(double));
+	if(NULL == k){
+		status = MALLOC_FAILED_CODE;
+		get_error_message(status);
+		goto l_cleanup;
+	}
+	status = graph_loading(argc, argv, &A, &k, &M);
+	printf("total number of vertices %d,", A->n);
+	status = SUCCESS_STATUS_CODE;
+	return 0;
+
+	l_cleanup:
+	exit(status);
+}
